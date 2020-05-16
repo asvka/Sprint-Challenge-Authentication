30be28e963ceb8b188190d5b9be1df1bec1f45ef
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-05-15 17:46:21 -0700

    built auth model, wrote auth router, and authenticate middleware. Register and Login are functional.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser')
+
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -11,6 +13,7 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser())
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
